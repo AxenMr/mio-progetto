bf84cf60cd3eda70a6c5944d26eaabf26b2b30f1
AxenMr <matteochiappa5@gmail.com>
2026-04-20 21:59:09 +0200

    Create Es6b.js

---
+++ Esercizi_6/Es6b.js
@@ -0,0 +1,6 @@
+calcolatrice();
+
+let numero = Number(prompt("Inserisci un numero intero da invertire:"));
+let risultatoInvertito = inverti_cifre(numero);
+
+alert("Numero invertito: " + risultatoInvertito);
