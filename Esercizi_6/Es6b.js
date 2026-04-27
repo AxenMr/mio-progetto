39ce5f17ce8c9ca69c8be392a7b8faf2cfac2eb1
AxenMr <matteochiappa5@gmail.com>
2026-04-27 22:22:43 +0200

    Update Es6b.js

--- Esercizi_6/Es6b.js
+++ Esercizi_6/Es6b.js
@@ -1,6 +1,25 @@
-calcolatrice();
+const readline = require("readline");
 
-let numero = Number(prompt("Inserisci un numero intero da invertire:"));
-let risultatoInvertito = inverti_cifre(numero);
+function inverti_cifre(n) {
+    const segno = Math.sign(n);
+    const invertito = parseInt(Math.abs(n).toString().split("").reverse().join(""));
+    return segno * invertito;
+}
 
-alert("Numero invertito: " + risultatoInvertito);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Inserisci un numero intero: ", (input) => {
+    const numero = parseInt(input);
+
+    if (isNaN(numero)) {
+        console.log("Errore: devi inserire un numero valido.");
+    } else {
+        const risultato = inverti_cifre(numero);
+        console.log("Numero invertito:", risultato);
+    }
+
+    rl.close();
+});
